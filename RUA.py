89f462850154a2749dcc44e886e09f28e9c1f8b7
AKASH KING <255928724+AKASH-KING-1@users.noreply.github.com>
2026-08-15 19:49:31 +0600

    Update RUA.py

--- RUA.py
+++ RUA.py
@@ -1,13 +1,9 @@
 import sys
 
 try:
-    import app
+    import LL
 except ImportError:
     sys.exit()
 
 if __name__ == "__main__":
-    app.approval()
-    try:
-        import LL
-    except ImportError:
-        sys.exit()
+    LL.main1()
